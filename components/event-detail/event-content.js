8e17d17214f22b680272a9d1d1d2a2927a2f10a2
gabrielraymond <gabrielraymond.grd@gmail.com>
2022-06-07 22:40:49 +0700

    optimizing nextjs apps

---
+++ components/event-detail/event-content.js
@@ -0,0 +1,9 @@
+import classes from "./event-content.module.css";
+
+
+const EventContent = (props) => {
+  const { text } = props;
+  return <section className={classes.content}>{props.children}</section>;
+};
+
+export default EventContent;
---
+++ pages/events/index.js
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../helpers/api-util";
+import EventsSearch from "../../components/events/events-search";
+
+const AllEventsPage = (props) => {
+  const router = useRouter();
+  const { events } = props;
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+  return (
+    <Fragment>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+};
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
+
+export default AllEventsPage;
